7b31ca61450767e358af8c79dc481e08503f0cae
JohaoMalfatti <110315050+JohaoMalfatti@users.noreply.github.com>
2022-08-13 16:43:39 -0300

    comandosprint

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("tees")
+from os import sep
+
+
+print('Johao', 'Luiz', sep= '-', end='') # sep - seprador no caso - entre as palavras 
+print('Vittoria', 'e', 'Maria', sep='-', end=' ***') #end unifica as linhas funciona também como separador
+
+print ('824','176','080', sep='.', end='-')
+print ('18')
